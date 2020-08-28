05b5b344c265f629b60af3e5a55af59be2760734
Shruti Joshi <shrutijoshi98@gmail.com>
2020-08-28 16:40:54 +0200

    rmed force sensor enabling

--- python/rrc_simulation/sim_finger.py
+++ python/rrc_simulation/sim_finger.py
@@ -537,12 +537,6 @@ class SimFinger:
         self.__load_stage()
         self.__disable_pybullet_velocity_control()
 
-        # enable force sensor on tips
-        for joint_index in self.pybullet_tip_link_indices:
-            pybullet.enableJointForceTorqueSensor(
-                self.finger_id, joint_index, enableSensor=True
-            )
-
     def __set_pybullet_params(self):
         """
         To change properties of the robot such as its mass, friction, damping,
